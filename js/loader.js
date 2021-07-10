0c5ab71c29a58cdaf7c7b7bd8a043b9bfcf9b095
Alex Volyk <alexvolyka@gmail.com>
2021-07-10 18:05:36 +0300

    Portfolio

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,13 @@
 window.addEventListener('load', () => {
+    let b = document.body.style;
     let mask = document.querySelector('.mask');
+
     mask.classList.add('hide');
     setTimeout(() => {
+        b.overflowY = 'overlay';
+        b.postion = "relative";
+
         mask.remove();
-    },600)
+    }, 1000)
 });
+
